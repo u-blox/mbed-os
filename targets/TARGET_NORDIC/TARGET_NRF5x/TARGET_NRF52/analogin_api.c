ff520d25130b51dc446d75544fc55e024f583c2c
Rob Meades <rob.meades@u-blox.com>
2018-11-01 10:25:20 +0000

    Set the acquire time for the ADC to 40 uS since this is required with the very low value of resistors (800 kOhm) that we are using.

--- targets/TARGET_NORDIC/TARGET_NRF5x/TARGET_NRF52/analogin_api.c
+++ targets/TARGET_NORDIC/TARGET_NRF5x/TARGET_NRF52/analogin_api.c
@@ -87,7 +87,7 @@ void analogin_init(analogin_t *obj, PinName pin)
         .resistor_n = NRF_SAADC_RESISTOR_DISABLED,
         .gain       = NRF_SAADC_GAIN1_4,
         .reference  = NRF_SAADC_REFERENCE_VDD4,
-        .acq_time   = NRF_SAADC_ACQTIME_10US,
+        .acq_time   = NRF_SAADC_ACQTIME_40US,
         .mode       = NRF_SAADC_MODE_SINGLE_ENDED,
         .burst      = NRF_SAADC_BURST_DISABLED,
         .pin_p      = input,
